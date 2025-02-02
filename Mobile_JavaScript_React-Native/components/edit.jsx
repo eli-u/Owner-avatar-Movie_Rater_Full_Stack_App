55470b37f86d34b78fd61869f84052b8dbfd6aa2
Eli Ugbomeh <eugbomeh@gmail.com>
2025-02-01 21:46:59 -0700

    Added new mobile fearture: User can now rate a selected movie on mobile.

--- Mobile_JavaScript_React-Native/components/edit.jsx
+++ Mobile_JavaScript_React-Native/components/edit.jsx
@@ -1,47 +1,49 @@
-import { useEffect } from 'react';
-import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button, TouchableOpacity, TextInput } from 'react-native';
+import { API_URL, API_TOKEN } from '@env';
 
 export default function Edit({ route, navigation }) {
 
     const { movie } = route.params;
+    const [title, setTitle] = useState(movie.title);
+    const [description, setDescription] = useState(movie.description);
 
-    useEffect(() => {
-        navigation.setOptions({
-            title: movie.title,
-            headerStyle: {
-                backgroundColor: 'orange'
+    const saveMovie = () => {
+        fetch(`${API_URL}/api/movies/${movie.id}/`, {
+            method: "PUT",
+            headers: {
+                'Authorization': `Token ${API_TOKEN}`,
+                'Content-Type': 'application/json'
             },
-            headerTitleStyle: {
-                fontWeight: 'bold',
-                fontSize: 24,
-                // color: 'white',
-            },
-            headerTitleAlign: 'center',
-            headerTintColor: 'white',
-            headerRight: () => (
-                <TouchableOpacity
-                    style={{
-                        backgroundColor: 'orange',
-                        paddingVertical: 6,
-                        paddingHorizontal: 12,
-                        borderRadius: 5
-                    }}
-                >
-                    <Text
-                        style={{
-                            color: 'white',
-                            fontSize: 18,
-                            fontWeight: 'bold'
-                        }}
-                    >Edit</Text>
-                </TouchableOpacity>
-            )
-        });
-    }, [movie, navigation]);
+            body: JSON.stringify({ title: title, description: description })
+        })
+            .then(res => res.json())
+            .then(movie => {
+                navigation.navigate("Detail", { movie: movie, title: movie.title });
+            })
+            .catch(error => console.log(error));
+    }
 
     return (
         <View style={styles.container}>
-            <Text style={styles.description}>Edit {movie.title}</Text>
+            <Text style={styles.label}>Title</Text>
+            <TextInput
+                style={styles.input}
+                placeholder='Title'
+                onChangeText={text => setTitle(text)}
+                value={title}
+            />
+            <Text style={styles.label}>Description</Text>
+            <TextInput
+                style={styles.input}
+                placeholder='Description'
+                onChangeText={text => setDescription(text)}
+                value={description}
+            />
+            <Button
+                onPress={() => saveMovie()}
+                title='Save'
+            />
         </View>
     );
 }
@@ -52,9 +54,15 @@ const styles = StyleSheet.create({
         backgroundColor: '#282C35',
         padding: 10
     },
-    description: {
-        fontSize: 20,
+    label: {
+        fontSize: 24,
         color: 'white',
         padding: 10
+    },
+    input: {
+        fontSize: 24,
+        backgroundColor: 'white',
+        padding: 10,
+        margin: 10
     }
 });
